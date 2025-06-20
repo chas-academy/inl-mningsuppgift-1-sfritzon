f822196bc47c53106c352e7fb7050a8de6709b51
sfritzon@gmail.com <sfritzon@gmail.com>
2025-06-20 21:42:23 +0200

    commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
     int dice_rolls[100];
-
+    
     for (int i=0; i<100; i++){
         dice_rolls[i] = (rand() %6) +1;
     }
